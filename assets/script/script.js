d6af3592ad772238e973afb24f3da3b703f92bdc
miladesmailpour <milad.esmailpour@gmail.com>
2023-05-04 18:11:01 -0400

    commentong out the code

--- assets/script/script.js
+++ assets/script/script.js
@@ -17,7 +17,7 @@ generateBtn.addEventListener("click", writePassword);
 function criteriaSelector() {
     // list of Criteria to ask
     var questions = [
-        "Please enter the length of the password!\nMust be a number and 8 is a minimum.",
+        "Please enter the length of the password!\nMust be a number, 8 is a min and 128 max128 .",
         "Do you want Lowercase character in password?",
         "How about the Uppercase?",
         "is there any number in you password?",
@@ -40,9 +40,7 @@ function criteriaSelector() {
         // store user input "Criteras"
         for (var i = 1; i < questions.length; i++) {
             criteria[i] = confirm(questions[i])
-            console.log(criteria[i])
         }
-        console.log(criteria[i])
         if (criteria.includes(true)) {
             inValidCriteria = false
         } else {
@@ -52,53 +50,75 @@ function criteriaSelector() {
     return criteria
 }
 
-function generatePassword(userinput) {
+// Password generator
+function generatePassword() {
+    var userinput = criteriaSelector()
     var len = userinput[0]
     var criteria = []
+    var usedCriteria = []
+    var randomCriteria
+    var randomChar
     var passwordArray = []
+    // defind the criteria
     if (userinput[1]) { criteria.push('l') }
     if (userinput[2]) { criteria.push('u') }
     if (userinput[3]) { criteria.push('n') }
     if (userinput[4]) { criteria.push('s') }
 
+    // Generate the password
     for (var i = 0; i <= len; i++) {
-
-        var randomCriteria = randomCharacter(criteria.length, "none")
-
-        var randomChar = randomCharacter(0, criteria[randomCriteria])
+        randomCriteria = randomCharacter(criteria.length, "none")
+        // kepp travking of applied criteria
+        if (!usedCriteria.includes(criteria[randomCriteria])) {
+            usedCriteria.push(criteria[randomCriteria])
+        }
+        // checking for all criteria to apply at least once
+        if (criteria.length === i && usedCriteria.length < criteria.length) {
+            for (var i = 0; i < criteria.length; i++) {
+                if (!usedCriteria.includes(criteria[i])) {
+                    randomCriteria = i
+                    usedCriteria.push(criteria[i])
+                }
+            }
+        }
+        randomChar = randomCharacter(0, criteria[randomCriteria])
         passwordArray.push(randomChar)
     }
-
-
     return passwordArray.join("")
-
 }
 
+// Random Character maker
 function randomCharacter(num, option) {
     var max = num
     var randomNum = 0
-    var specialChar = ["~", "!", "@", "#", "$", "%", "&", "*", "(", ")", "_", "-", "+", "<", ">", "?", "/", "\\", "{", "}", "[", "]"]
+    var specialChar = ["~", "!", "@", "#", "$", "%", "&", "*", "(", ")",
+        "_", "-", "+", "<", ">", "?", "/", "\\", "{", "}", "[", "]"]
+
     if (option == "none") {
         return Math.floor(Math.random() * max)
     }
+    // lowercase char
     if (option == "l") {
         max = 25
         randomNum = Math.floor(Math.random() * max)
         randomNum += 97
         return String.fromCharCode(randomNum)
     }
+    // uppercase char
     if (option == "u") {
         max = 25
         randomNum = Math.floor(Math.random() * max)
         randomNum += 65
         return String.fromCharCode(randomNum)
     }
+    // number
     if (option == "n") {
         max = 9
         randomNum = Math.floor(Math.random() * max)
         randomNum += 48
         return String.fromCharCode(randomNum)
     }
+    // special char
     if (option == "s") {
         max = specialChar.length
         randomNum = Math.floor(Math.random() * max)
@@ -106,5 +126,4 @@ function randomCharacter(num, option) {
     }
 
 }
-console.log(generatePassword(criteriaSelector()))
-writePassword()
+
