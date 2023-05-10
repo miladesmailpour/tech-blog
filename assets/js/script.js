2f6a25f1fa4b089c592a081962b57d83db2e145c
miladesmailpour <milad.esmailpour@gmail.com>
2023-05-10 00:05:50 -0400

    add data.js content to script.js for github deploy error handeling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,224 +1,4 @@
 
-var bodyEl = document.querySelector("body")
-var userName = document.querySelector('#user-name')
-var userNameDisplay = document.querySelector('#user-name-display')
-var ulEl = document.querySelector('#multi-choices')
-var quizSubjects = ['js', 'css', 'html']
-var HIGH_SCORE = 'high-score'
-var START = 'start'
-var START_OVER = 'start-over'
-var NEXT = 'next'
-var userInfo = {
-    name: '',
-    score: 0
-}
-var mChoice = document.querySelector('#multi-choices')
-var views = {
-    welcome: document.querySelector('#welcome-view'),
-    start: document.querySelector('#start-view'),
-    quiz: document.querySelector('#quiz-view'),
-    result: document.querySelector('#result-view'),
-    highest: document.querySelector('#high-score-view')
-}
-var answredQuestion = []
-var questionTemplate = {
-    question: document.querySelector('#question'),
-    choice1: mChoice.children[0],
-    choice2: mChoice.children[1],
-    choice3: mChoice.children[2],
-    choice4: mChoice.children[3],
-    index: mChoice.children[4]
-}
-var next = document.querySelector('#next-question')
-var questions
-
-// listening to the clicked event button and li 
-// and navigeting between the views
-bodyEl.addEventListener('click', viewHandler)
-function viewHandler(event) {
-    event.preventDefault();
-    var ele = event.target
-    var dataAttr
-    questions = []
-
-    if (ele.matches('button')) {
-        dataAttr = ele.getAttribute('data-btn')
-        if (quizSubjects.includes(dataAttr)) {
-            inital(dataAttr)
-
-        }
-        else if (dataAttr === HIGH_SCORE) {
-            displayView(views.highest, true)
-            displayView(views.welcome, false)
-            displayView(views.result, false)
-
-        } else if (dataAttr === START) {
-            start(dataAttr)
-
-        } else if (dataAttr === START_OVER) {
-            startOver(dataAttr)
-        } else if (dataAttr === NEXT) {
-            nextQuestion()
-        } else { return }
-    }
-    if (ele.matches('li')) {
-
-        selectorHandler(ele)
-    }
-}
-
-function displayView(view, option) {
-    if (option) {
-        view.setAttribute('style', 'display: block;')
-    }
-    else {
-        view.setAttribute('style', 'display: none;')
-    }
-}
-
-function inital(subject) {
-    if (subject == quizSubjects[0]) {
-        questions = jsQuestions
-    }
-    if (subject == quizSubjects[1]) {
-        questions = cssQuestions
-    }
-    if (subject == quizSubjects[2]) {
-        questions = htmlQuestions
-    }
-    quizLocalStorage("questions", questions, 'w')
-
-    displayView(views.start, true)
-    displayView(views.welcome, false)
-}
-
-function start(subject) {
-    userInfo.name = userName.value
-    quizLocalStorage("userInfo", userInfo, 'w')
-    userNameDisplay.textContent = userInfo.name
-    displayView(views.start, false)
-    displayView(views.quiz, true)
-    nextQuestion()
-}
-
-function startOver(subject) {
-    displayView(views.result, false)
-    displayView(views.highest, false)
-    displayView(views.welcome, true)
-}
-
-function quizLocalStorage(name, store, state) {
-    var quizzes = JSON.parse(localStorage.getItem('quizzes'))
-    quizzes[name] = store
-    // console.log(quizzes)
-    if (state == 'w') {
-
-        localStorage.setItem('quizzes', JSON.stringify(quizzes))
-    }
-    if (state == 'r') {
-        quizzes = JSON.parse(localStorage.getItem('quizzes'))
-        questions = quizzes.questions
-        userInfo = quizzes.userInfo
-    }
-    else { return }
-
-}
-
-function nextQuestion() {
-    next.disabled = true
-    for (var i = 0; i < 4; i++) {
-        views.quiz.children[1].children[i].setAttribute("style", "background-color: var(--font-dark); color: var(--font-light);")
-    }
-
-
-    quizLocalStorage("questions", questions, 'r')
-    if (questions === null) {
-        return
-    }
-    // console.log(answredQuestion.length, questions.length)
-
-    if (answredQuestion.length < questions.length) {
-        var currentQuestion = questions[answredQuestion.length]
-        answredQuestion.push(questions[answredQuestion.length])
-
-        questionTemplate.question.textContent = currentQuestion.question
-        questionTemplate.choice1.textContent = currentQuestion.a
-        questionTemplate.choice2.textContent = currentQuestion.b
-        questionTemplate.choice3.textContent = currentQuestion.c
-        questionTemplate.choice4.textContent = currentQuestion.d
-        questionTemplate.index.textContent = currentQuestion.index
-    }
-    else {
-        displayView(views.quiz, false)
-        displayView(views.result, true)
-
-        views.result.children[1].textContent = userInfo.name + ": " + userInfo.score
-
-        answredQuestion.length = 0
-    }
-}
-
-function selectorHandler(currentEle) {
-
-    var choice = currentEle.dataset.multiChoice
-    var index = currentEle.parentElement.lastElementChild.dataset.multiChoice
-
-    quizLocalStorage("questions", questions, 'r')
-
-    // console.log("+" + choice, index, questions)
-    if (index = 'index') {
-        index = currentEle.parentElement.lastElementChild.textContent
-        // console.log(index)
-        for (var i = 0; i < questions.length; i++) {
-            if (questions[i].index == index) {
-                if (choice == questions[i].answer) {
-                    // console.log("correct " + choice, questions[i].answer)
-                    quizLocalStorage("userInfo", userInfo, 'r')
-                    userInfo.score++
-                    quizLocalStorage("userInfo", userInfo, 'w')
-                    // console.log(userInfo)
-                    choiceLock(choice, questions[i].answer)
-                }
-                else {
-                    // console.log("wrong " + choice, questions[i].answer)
-                    choiceLock(choice, questions[i].answer)
-                }
-            }
-        }
-        next.disabled = false
-
-    }
-
-}
-
-function choiceLock(choice, correct) {
-    // console.log(choice, correct)
-    if (correct != choice) {
-        for (var i = 0; i < 4; i++) {
-            if (views.quiz.children[1].children[i].getAttribute('data-multi-choice') == correct) {
-                views.quiz.children[1].children[i].setAttribute("style", "background-color: green;")
-            }
-            else if (views.quiz.children[1].children[i].getAttribute('data-multi-choice') == choice) {
-                views.quiz.children[1].children[i].setAttribute("style", "background-color: red;")
-            }
-            else {
-                views.quiz.children[1].children[i].setAttribute("style", "background-color: grey; color: var(--font-light);")
-            }
-        }
-    } else {
-        for (var i = 0; i < 4; i++) {
-            if (views.quiz.children[1].children[i].getAttribute('data-multi-choice') == correct) {
-                views.quiz.children[1].children[i].setAttribute("style", "background-color: green;")
-            }
-            else {
-                views.quiz.children[1].children[i].setAttribute("style", "background-color: grey; color: var(--font-light);")
-            }
-        }
-    }
-
-}
-
-
 // Questions: Array of Javascript Object which each includes question, multiple choices, and correct answer 
 
 var jsQuestions = [
@@ -475,3 +255,225 @@ var htmlQuestions = [{
     answer: "a",
     index: "10"
 }]
+var bodyEl = document.querySelector("body")
+var userName = document.querySelector('#user-name')
+var userNameDisplay = document.querySelector('#user-name-display')
+var ulEl = document.querySelector('#multi-choices')
+var quizSubjects = ['js', 'css', 'html']
+var HIGH_SCORE = 'high-score'
+var START = 'start'
+var START_OVER = 'start-over'
+var NEXT = 'next'
+var userInfo = {
+    name: '',
+    score: 0
+}
+var mChoice = document.querySelector('#multi-choices')
+var views = {
+    welcome: document.querySelector('#welcome-view'),
+    start: document.querySelector('#start-view'),
+    quiz: document.querySelector('#quiz-view'),
+    result: document.querySelector('#result-view'),
+    highest: document.querySelector('#high-score-view')
+}
+var answredQuestion = []
+var questionTemplate = {
+    question: document.querySelector('#question'),
+    choice1: mChoice.children[0],
+    choice2: mChoice.children[1],
+    choice3: mChoice.children[2],
+    choice4: mChoice.children[3],
+    index: mChoice.children[4]
+}
+var next = document.querySelector('#next-question')
+var questions
+
+// listening to the clicked event button and li 
+// and navigeting between the views
+bodyEl.addEventListener('click', viewHandler)
+function viewHandler(event) {
+    event.preventDefault();
+    var ele = event.target
+    var dataAttr
+    questions = []
+
+    if (ele.matches('button')) {
+        dataAttr = ele.getAttribute('data-btn')
+        if (quizSubjects.includes(dataAttr)) {
+            inital(dataAttr)
+
+        }
+        else if (dataAttr === HIGH_SCORE) {
+            displayView(views.highest, true)
+            displayView(views.welcome, false)
+            displayView(views.result, false)
+
+        } else if (dataAttr === START) {
+            start(dataAttr)
+
+        } else if (dataAttr === START_OVER) {
+            startOver(dataAttr)
+        } else if (dataAttr === NEXT) {
+            nextQuestion()
+        } else { return }
+    }
+    if (ele.matches('li')) {
+
+        selectorHandler(ele)
+    }
+}
+
+function displayView(view, option) {
+    if (option) {
+        view.setAttribute('style', 'display: block;')
+    }
+    else {
+        view.setAttribute('style', 'display: none;')
+    }
+}
+
+function inital(subject) {
+    if (subject == quizSubjects[0]) {
+        questions = jsQuestions
+    }
+    if (subject == quizSubjects[1]) {
+        questions = cssQuestions
+    }
+    if (subject == quizSubjects[2]) {
+        questions = htmlQuestions
+    }
+    quizLocalStorage("questions", questions, 'w')
+
+    displayView(views.start, true)
+    displayView(views.welcome, false)
+}
+
+function start(subject) {
+    userInfo.name = userName.value
+    quizLocalStorage("userInfo", userInfo, 'w')
+    userNameDisplay.textContent = userInfo.name
+    displayView(views.start, false)
+    displayView(views.quiz, true)
+    nextQuestion()
+}
+
+function startOver(subject) {
+    displayView(views.result, false)
+    displayView(views.highest, false)
+    displayView(views.welcome, true)
+}
+
+function quizLocalStorage(name, store, state) {
+    var quizzes = JSON.parse(localStorage.getItem('quizzes'))
+    quizzes[name] = store
+    // console.log(quizzes)
+    if (state == 'w') {
+
+        localStorage.setItem('quizzes', JSON.stringify(quizzes))
+    }
+    if (state == 'r') {
+        quizzes = JSON.parse(localStorage.getItem('quizzes'))
+        questions = quizzes.questions
+        userInfo = quizzes.userInfo
+    }
+    else { return }
+
+}
+
+function nextQuestion() {
+    next.disabled = true
+    for (var i = 0; i < 4; i++) {
+        views.quiz.children[1].children[i].setAttribute("style", "background-color: var(--font-dark); color: var(--font-light);")
+    }
+
+
+    quizLocalStorage("questions", questions, 'r')
+    if (questions === null) {
+        return
+    }
+    // console.log(answredQuestion.length, questions.length)
+
+    if (answredQuestion.length < questions.length) {
+        var currentQuestion = questions[answredQuestion.length]
+        answredQuestion.push(questions[answredQuestion.length])
+
+        questionTemplate.question.textContent = currentQuestion.question
+        questionTemplate.choice1.textContent = currentQuestion.a
+        questionTemplate.choice2.textContent = currentQuestion.b
+        questionTemplate.choice3.textContent = currentQuestion.c
+        questionTemplate.choice4.textContent = currentQuestion.d
+        questionTemplate.index.textContent = currentQuestion.index
+    }
+    else {
+        displayView(views.quiz, false)
+        displayView(views.result, true)
+
+        views.result.children[1].textContent = userInfo.name + ": " + userInfo.score
+
+        answredQuestion.length = 0
+    }
+}
+
+function selectorHandler(currentEle) {
+
+    var choice = currentEle.dataset.multiChoice
+    var index = currentEle.parentElement.lastElementChild.dataset.multiChoice
+
+    quizLocalStorage("questions", questions, 'r')
+
+    // console.log("+" + choice, index, questions)
+    if (index = 'index') {
+        index = currentEle.parentElement.lastElementChild.textContent
+        // console.log(index)
+        for (var i = 0; i < questions.length; i++) {
+            if (questions[i].index == index) {
+                if (choice == questions[i].answer) {
+                    // console.log("correct " + choice, questions[i].answer)
+                    quizLocalStorage("userInfo", userInfo, 'r')
+                    userInfo.score++
+                    quizLocalStorage("userInfo", userInfo, 'w')
+                    // console.log(userInfo)
+                    choiceLock(choice, questions[i].answer)
+                }
+                else {
+                    // console.log("wrong " + choice, questions[i].answer)
+                    choiceLock(choice, questions[i].answer)
+                }
+            }
+        }
+        next.disabled = false
+
+    }
+
+}
+
+function choiceLock(choice, correct) {
+    // console.log(choice, correct)
+    if (correct != choice) {
+        for (var i = 0; i < 4; i++) {
+            if (views.quiz.children[1].children[i].getAttribute('data-multi-choice') == correct) {
+                views.quiz.children[1].children[i].setAttribute("style", "background-color: green;")
+            }
+            else if (views.quiz.children[1].children[i].getAttribute('data-multi-choice') == choice) {
+                views.quiz.children[1].children[i].setAttribute("style", "background-color: red;")
+            }
+            else {
+                views.quiz.children[1].children[i].setAttribute("style", "background-color: grey; color: var(--font-light);")
+            }
+        }
+    } else {
+        for (var i = 0; i < 4; i++) {
+            if (views.quiz.children[1].children[i].getAttribute('data-multi-choice') == correct) {
+                views.quiz.children[1].children[i].setAttribute("style", "background-color: green;")
+            }
+            else {
+                views.quiz.children[1].children[i].setAttribute("style", "background-color: grey; color: var(--font-light);")
+            }
+        }
+    }
+
+}
+
+
+
+
