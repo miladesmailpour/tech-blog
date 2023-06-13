52e225a9aa1c5d1ba7e51d51d8815d9f512f83fd
miladesmailpour <milad.esmailpour@gmail.com>
2023-06-12 23:35:52 -0400

    Added the session and sequilize to save in db as well as running the app inside sequlize sync

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
 const path = require('path');
 const express = require('express');
 const routes = require('./controllers');
+const session = require('express-session');
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -11,6 +31,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
-app.listen(PORT, () =>
-  console.log('Now listening on http://localhost:' + PORT)
-);
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () =>
+    console.log('Now listening on http://localhost:' + PORT)
+  );
+});
