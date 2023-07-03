5b6e9d1494ae7d50dabd850c80c46ba30124065f
miladesmailpour <milad.esmailpour@gmail.com>
2023-07-03 00:46:43 -0400

    get rooutes added to homeRoutes to show all the post and their comments

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,27 +1,28 @@
 const router = require('express').Router();
-const { Post, Comment } = require('../models');
+const { Post, Comment, User } = require('../models');
 
 router.get('/', async (req, res) => {
   try {
-    // Get all Posts and JOIN with user data
     const postData = await Post.findAll({
-      include: [
-        {
+      attributes: ['id', 'title', 'description', 'date_created'],
+      include: [{
           model: Comment,
-          attributes: ['text', 'date_created'],
+          attributes: ['id', 'text', 'date_created', 'post_id', 'user_id'],
+          include: {
+            model: User,
+            attributes: ['name']
+          }
         },
-      ],
+        {
+          model: User,
+          attributes: ['name']
+        }
+      ]
     });
-
-    // Serialize data so the template can read it
-    const posts = postData.map((post) => post.get({ plain: true }));
-
-    // Pass serialized data and session flag into template
-    res.render('homepage', {
-      posts,
-      logged_in: req.session.logged_in,
-    });
-  } catch (err) {
+    const posts = postData.map(post => post.get({ plain: true }));
+    res.render('homepage', { posts, logged_in: req.session.logged_in }); // homepage view should be implement
+  } 
+  catch (err) {
     res.status(500).json(err);
   }
 });
