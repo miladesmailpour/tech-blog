de9f3a6111e9def8f76dac602ed70b48245e531d
miladesmailpour <milad.esmailpour@gmail.com>
2023-07-03 01:07:55 -0400

    add get route to the home page to get comment by id

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -109,7 +109,9 @@ router.get('/profile', withAuth, async (req, res) => {
   try {
     const userData = await User.findByPk(req.session.user_id, {
       attributes: { exclude: ['password'] },
-      include: [{ model: Post }],
+      include: [{ 
+        model: Post 
+      }],
     });
     const user = userData.get({ plain: true });
     res.render('profile', { ...user, logged_in: true }); // profile view should be implement
@@ -118,4 +120,39 @@ router.get('/profile', withAuth, async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+router.get('/comment/:id', async (req, res) => {
+  try {
+    const postData = await Post.findOne({
+      where: {
+        id: req.params.id
+      },
+      attributes: ['id', 'title', 'description', 'date_created'],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'text', 'date_created', 'post_id', 'user_id'],
+          include: {
+            model: User,
+            attributes: ['name']
+          }
+        },
+        {
+          model: User,
+          attributes: ['name']
+        }
+      ]
+    });
+    if (!postData) {
+      res.status(404).json({ message: `post with id: ${req.params.id} NOT found.` });
+      return;
+    }
+    const post = postData.get({ plain: true });
+    res.render('comment', { post, logged_in: req.session.logged_in });
+  } 
+  catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
