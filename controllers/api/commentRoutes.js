8f3a56e0372bff39a5e924c8861bc722f4946d1e
miladesmailpour <milad.esmailpour@gmail.com>
2023-07-02 23:05:02 -0400

    post route added ti create a comment

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const {Comment}  = require('../../models');
+const withAuth = require('../../utils/auth');
 
 router.get('/', async (req, res) => {
     try {
@@ -22,6 +23,20 @@ router.get('/:id', async (req, res) => {
     } 
     catch (err) {
         res.status(500).json(err);
+    }
+});
+
+router.post('/', withAuth, async (req, res) => {
+    if (req.session) {
+        try {
+            const commentData = await Comment.create({
+                ...req.body
+            });
+            res.json(commentData);
+        } 
+        catch (err) {
+            res.status(400).json(err);
+        }
     }
 });
 
